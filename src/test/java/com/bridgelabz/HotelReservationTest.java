01c6cc489331c50a2e9c31840c964106410f9c0f
CMD SHAKEEL <cmdshakeel21@gmail.com>
2021-08-09 09:21:51 +0530

    WELCOME_TO_HOTEL_RESERVATION_SYSTEM

---
+++ src/test/java/com/bridgelabz/HotelReservationTest.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+
+import org.junit.Test;
+
+// creating HotelReservation Test class
+public class HotelReservationTest {
+
+    @Test
+    public void givenHotelNameAndRatesShouldReturnTrue() {
+
+       /*
+       creating a object of hotelReservation
+        */
+        HotelReservation hotelReservation = new HotelReservation();
+        hotelReservation.add("BridgeWood",160,60);
+        hotelReservation.add("LikeWood",110,90);
+        hotelReservation.add("RidgeWood",220,150);
+
+    }
+}
