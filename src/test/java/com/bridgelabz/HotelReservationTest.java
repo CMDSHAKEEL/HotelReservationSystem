1665480eafb623af583c6c4360097a021993a89a
CMD SHAKEEL <cmdshakeel21@gmail.com>
2021-08-10 19:11:49 +0530

    Added:-weekenddays and weekrates of each hotel

--- src/test/java/com/bridgelabz/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/HotelReservationTest.java
@@ -28,9 +28,9 @@ public class HotelReservationTest {
        creating a object of hotelReservation
         */
         HotelReservation hotelReservation = new HotelReservation();
-        hotelReservation.add("BridgeWood", 60, 160);
-        hotelReservation.add("LikeWood", 100, 90);
-        hotelReservation.add("RidgeWood", 80, 150);
+        hotelReservation.add("BridgeWood", 160, 60);
+        hotelReservation.add("LikeWood", 110, 90);
+        hotelReservation.add("RidgeWood", 220, 150);
 
 
         Assert.assertEquals(160,HotelReservation.listOfHotels.get("BridgeWood").getRateRegularCustomer());
@@ -45,10 +45,26 @@ public class HotelReservationTest {
     public void givenDatesShouldReturnCheapestHotel() throws ParseException {
         HotelReservation hotelReservation = new HotelReservation();
         hotelReservation.add("BridgeWood",160,60);
-        hotelReservation.add("LikeWood",110,90);
+        hotelReservation.add("LikeWood",100,90);
         hotelReservation.add("RidgeWood",220,150);
         String cheapestHotel = hotelReservation.findCheapestHotel("10sep2020", "11sep2020");
         Assert.assertEquals("LikeWood",cheapestHotel);
+
+    }
+    //creating weekdays and weekrates method
+
+    @Test
+    public void givenWeekDaysAndWeekendRatesShouldReturnTrue() {
+        HotelReservation hotelReservation = new HotelReservation();
+        hotelReservation.add("BridgeWood",160,60);
+        hotelReservation.add("LikeWood",110,90);
+        hotelReservation.add("RidgeWood",220,150);
+        Assert.assertEquals(160,HotelReservation.listOfHotels.get("BridgeWood").getRateRegularCustomer());
+        Assert.assertEquals(60,HotelReservation.listOfHotels.get("BridgeWood").getRateRegularWeekend());
+        Assert.assertEquals(110,HotelReservation.listOfHotels.get("LikeWood").getRateRegularCustomer());
+        Assert.assertEquals(90,HotelReservation.listOfHotels.get("LikeWood").getRateRegularWeekend());
+        Assert.assertEquals(220,HotelReservation.listOfHotels.get("RidgeWood").getRateRegularCustomer());
+        Assert.assertEquals(150,HotelReservation.listOfHotels.get("RidgeWood").getRateRegularWeekend());
 
     }
 }
